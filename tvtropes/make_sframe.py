53ad3b59bbad945188fb9d920948d7ab146845bd
Annaliese Johnson <annajo1138@gmail.com>
2016-05-06 08:59:09 +0200

    sframes are the new matrices

---
+++ tvtropes/make_sframe.py
@@ -0,0 +1,77 @@
+import json
+import ast
+import scipy.io
+from scipy.sparse import dok_matrix
+
+def nameToUrl(string):
+    return string.strip()
+
+jsonfile = open("dumpfile")
+print "json file"
+
+jsonstr = jsonfile.read()
+print "json string"
+
+decoder = json.JSONDecoder()
+print "file opened"
+
+loaded = decoder.decode(jsonstr)
+print "loaded"
+
+jsonfile.close()
+
+dictfile = open("dictfile", 'r+')
+readdict = dictfile.read()
+nameTypeDict = ast.literal_eval(readdict)
+dictfile.close()
+
+works = {}
+tropes = {}
+
+workIndex = 0
+tropeIndex = 0
+
+for page,pageType in nameTypeDict.iteritems():
+    if pageType == "Trope":
+        tropes[page] = tropeIndex
+        tropeIndex += 1
+    if pageType == "Work":
+        works[page] = workIndex
+        workIndex += 1
+
+print "got index dictionaries"
+
+workindexfile = open("workindexfile", 'w')
+workjson = json.JSONEncoder().encode(works)
+workindexfile.write(workjson)
+workindexfile.close()
+
+tropeindexfile = open("tropeindexfile", 'w')
+tropejson = json.JSONEncoder().encode(tropes)
+tropeindexfile.write(tropejson)
+tropeindexfile.close()
+
+print "index files written"
+
+csv = open("sframe.csv", 'w')
+csv.write((u'title`trope`hasTrope\n').encode('utf8'))
+
+for page in loaded:
+    if page['pageType'] == "Work":
+        title = page['title'][0]
+        links = page['all_links']
+        for trope in tropes:
+            if trope in links:
+                towrite = title+u'`'+trope+u'`'+u"1\n"
+                csv.write(towrite.encode('utf8'))
+
+csv.close()
+
+#matrixfile = open("matrixfile", 'w')
+#print "matrix file opened"
+
+#scipy.io.mmwrite(matrixfile, matrix)
+#print "matrix written"
+
+#matrixfile.close()
+print "done"
