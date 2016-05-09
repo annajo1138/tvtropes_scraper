7b107dcf290894b859e82426761ddba0477b0c3c
Annaliese Johnson <annajo1138@gmail.com>
2016-05-09 10:20:35 +0200

    adding python script and txts of recommender outputs

---
+++ tvtropes/recommender.py
@@ -0,0 +1,23 @@
+import graphlab
+from numba import autojit
+
+def readfile():
+    return graphlab.SFrame.read_csv('sframe.csv', delimiter='`')
+
+sframe = readfile();
+
+data_train, data_test = graphlab.recommender.util.random_split_by_user(sframe, user_id='title', item_id='trope', item_test_proportion=0.2)
+
+@autojit
+def makeModel():
+    return graphlab.recommender.ranking_factorization_recommender.create(data_train, user_id='title', item_id='trope', target='hasTrope', solver='ials')
+
+model = makeModel();
+
+@autojit
+def doEval():
+    return model.evaluate(data_test, target='hasTrope')
+
+eva = doEval();
+
+print "DONE"
