c312c3ec298226b7a0d12534206994ae23e86d7a
Annaliese Johnson <annajo1138@gmail.com>
2016-05-13 10:51:00 +0200

    comments and more recommender stats

--- tvtropes/recommender.py
+++ tvtropes/recommender.py
@@ -1,16 +1,23 @@
 import graphlab
 from numba import autojit
 
+# numba/autojit is a python compiler that optimizes for Intel processors.
+# If you don't have it installed just delete the import and decorators.
+
 def readfile():
-    return graphlab.SFrame.read_csv('sframe_big.csv', delimiter='`')
+    return graphlab.SFrame.read_csv('sframe.csv', delimiter='`')
 
 sframe = readfile();
 
-data_train, data_test = graphlab.recommender.util.random_split_by_user(sframe, user_id='title', item_id='trope', item_test_proportion=0.2)
+# these are correct
+userid = 'title'
+itemid = 'trope'
+
+data_train, data_test = graphlab.recommender.util.random_split_by_user(sframe, user_id=userid, item_id=itemid, item_test_proportion=0.2)
 
 @autojit
 def makeModel():
-    return graphlab.recommender.ranking_factorization_recommender.create(data_train, user_id='title', item_id='trope', target='hasTrope', solver='sgd')
+    return graphlab.recommender.ranking_factorization_recommender.create(data_train, user_id=userid, item_id=itemid, target='hasTrope', solver='sgd')
 
 model = makeModel();
 
